dfefc7b1db7d000c2a5fcdb655af135cd1e9791c
Nyarll <tonuaa@yahoo.co.jp>
2018-10-12 12:00:34 +0900

    Init Commit

---
+++ Assets/ObjectRotate.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectRotate : MonoBehaviour {
+
+    public float speed = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(Input.GetKey(KeyCode.Space))
+        {
+            transform.Rotate(new Vector3(0, 0, speed * Time.deltaTime));
+        }	
+
+	}
+}
